76bd7935cf97d259b55b85b7980235d856f9e8b6
Yiwei Yang <40686366+victoryang00@users.noreply.github.com>
2023-10-26 12:57:08 -0700

    add wasm llamma

---
+++ wamr-ggml-llama/llama.cpp
@@ -0,0 +1,7 @@
+//
+// Created by victoryang00 on 10/26/2023.
+//
+
+#include "llama.h"
+
+int main() {}
---
+++ wamr-ggml-llama/llama.h
@@ -0,0 +1,10 @@
+//
+// Created by victoryang00 on 10/26/2023.
+//
+
+#ifndef MVVM_LLAMA_H
+#define MVVM_LLAMA_H
+
+class llama {};
+
+#endif // MVVM_LLAMA_H
